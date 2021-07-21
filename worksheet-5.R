aa29f13f924af9f5ec76d528fe3c68b73be4f166
jbalerna <jessicabalerna@gmail.com>
2021-07-21 21:11:44 +0000

    finished all code for this worksheet

--- worksheet-5.R
+++ worksheet-5.R
@@ -56,103 +56,137 @@ ggplot() +
 
 ## Coordinate Transforms
 
-shp <- 'data/huc250k'
-huc <- st_read(...)
+# this code can filter for overlapping spatial locations
+st_filter(counties_md, sesync)
 
+
+#Importing a new shapefile
+shp <- '~/data/huc250k'
+
+#Creating shapefile huc
+huc <- st_read(shp)
+
+st_crs(counties_md)
+st_crs(huc)
+
+#creates projection for transformation
 prj <- '+proj=aea +lat_1=29.5 +lat_2=45.5 \
         +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 \
         +datum=WGS84 +units=m +no_defs'
 
+#projects counties, huc, and sesync point to the same coordinate system
+#this can take awhile for big datasets
+
 counties_md <- st_transform(
     counties_md,
-    ...)
-huc <- ...(huc, ...)
-sesync <- ...
+    crs=prj)
+huc <- st_transform(huc, crs=prj)
+sesync <- st_transform(sesync, crs=prj)
 
-plot(st_geometry(...))
-plot(...,
+plot(st_geometry(counties_md)) #no fill for color - only borders
+plot(st_geometry(huc),
      border = 'blue', add = TRUE)
-plot(..., col = 'green',
+plot(sesync, col = 'green',
      pch = 20, add = TRUE)
 
 ## Geometric Operations
 
-state_md <- ...
-plot(...)
+state_md <- st_union(counties_md)
+plot(state_md)
 
-huc_md <- ...(..., ...)
+huc_md <- st_intersection(huc, state_md)
 
 plot(state_md)
-plot(..., border = 'blue',
+plot(st_geometry(huc_md), border = 'blue',
      col = NA, add = TRUE)
 
 ## Raster Data
 
-library(...)
-nlcd <- ...("data/nlcd_agg.tif", ...)
-nlcd <- droplevels(...)
+library(stars)
+nlcd <- read_stars("~/data/nlcd_agg.tif", proxy=FALSE)
+nlcd <- droplevels(nlcd)
+plot(nlcd)
 
 ## Crop
 
-md_bbox <- ...
-nlcd <- st_crop(..., ...)
+md_bbox <- st_bbox(huc_md)
+nlcd <- st_crop(nlcd, md_bbox) #crops using the bounding box for md_bbox data
+plot(nlcd)
 
 ggplot() +
-    ...(data = nlcd) +
-    ...(data = ..., fill = NA) +
-    scale_fill_manual(values = attr(nlcd[[1]], ...))
+    geom_stars(data = nlcd) +
+    geom_sf(data = huc_md, fill = NA) +
+    scale_fill_manual(values = attr(nlcd[[1]], "colors")) #this pulls out color scheme
 
 ## Raster math
 
+levels(nlcd[[1]])
+
 forest_types <- c('Evergreen Forest', 'Deciduous Forest', 'Mixed Forest')
 forest <- nlcd
-forest[...(... %in% ...)] <- ...
+forest[!(forest %in% forest_types)] <- NA #turns off all pixels that are not forests
+#! negates everything within a subsequent logical sequence
 plot(forest)
 
 ## Downsampling a raster
 
-nlcd_agg <- ...(nlcd,
-                ... = 1500, 
-                method = ..., 
-                use_gdal = TRUE)
+nlcd_agg <- st_warp(nlcd,
+                cellsize = 1500, 
+                method = "mode", 
+                use_gdal = TRUE) #gdal is a library needed for complex rasters
 
-nlcd_agg <- ...(nlcd_agg) 
-...(...[[1]]) <- ...(nlcd[[1]]) 
+nlcd_agg <- droplevels(nlcd_agg) 
+levels(nlcd_agg[[1]]) <- levels(nlcd[[1]]) 
 
-plot(...)
+plot(nlcd_agg) #bigger pixels, lower resolution
 
 ## Mixing rasters and vectors
 
-plot(nlcd, ... = FALSE)
+plot(nlcd, reset = FALSE)
 plot(sesync, col = 'green',
-     pch = 16, cex = 2, ...)
+     pch = 16, cex = 2, add=TRUE)
 
-sesync_lc <- ...(nlcd, ...)
+#tells us what nlcd land cover is at the sesync location
+sesync_lc <- st_extract(nlcd, sesync) 
+sesync_lc
 
-baltimore <- nlcd[...]
-plot(baltimore)
 
+baltimore_city <- nlcd[counties_md[1, ]] #first row of counties in MD = Baltimore
+plot(baltimore_city)
+
+#ALTERNATIVE CODE FOR THE SAME THING
+baltimore_city_alt <- st_crop(nlcd, counties_md[1, ])
+plot(baltimore_city_alt)
+
+#ONE MORE TIME - SAME THING, BUT USING "PIPELINE" TO GET A TABLE (INSTEAD OF MAP)
 nlcd %>%
-    ...(counties_md[1, ]) %>%
-    ... %>%
-    ...
+    st_crop(counties_md[1, ]) %>%
+    pull %>%
+    table
 
-mymode <- function(x) names(which.max(table(x)))
+mymode <- function(x) names(which.max(table(x))) #gets the most abundant value in table of qualatative values
 
-modal_lc <- aggregate(..., ..., ... = mymode) 
+modal_lc <- aggregate(nlcd_agg, huc_md, FUN = mymode) 
+modal_lc #this is hard to read
 
+st_as_sf(modal_lc) #this shows us that for each HUC (30 total) the most common LULC for each
+
+
+#let's map that!!
 huc_md <- huc_md %>% 
-    ...(modal_lc = ...)
+    mutate(modal_lc = modal_lc[[1]]) #adds outcomes from above to huc_md
 
-ggplot(..., aes(fill = ...)) + 
-    geom_sf()
+ggplot(huc_md, aes(fill = modal_lc)) + 
+    geom_sf() #this is cool
 
-## Mapview
+## Mapview - can add basemaps
 
-library(...)
-...(huc_md)
+library(mapview)
+mapview(huc_md)
 
-mapview(..., legend = FALSE, ... = 0.5, 
+mapview(nlcd_agg, legend = FALSE, alpha = 0.5, 
         map.types = 'OpenStreetMap') +
-    mapview(..., legend = FALSE, ... = 0.2)
+    mapview(huc_md, legend = FALSE, alpa = 0.2)
 
+#alpha determines transparency
+#this map is super ugly
